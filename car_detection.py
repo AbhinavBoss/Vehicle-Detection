ff14bc57d11e491ee16716634f6ae421c76c20e2
Abhinav Varshney <115035256+AbhinavBoss@users.noreply.github.com>
2023-11-03 00:12:30 +0530

    Update car_detection.py

--- car_detection.py
+++ car_detection.py
@@ -1,15 +1,3 @@
-#Car detection in image
-# import cv2
-# car_cascade = cv2.CascadeClassifier('cars.xml')
-# img = cv2.imread(r'D:\PROGRAMMING LANGUAGES\7.png')
-# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# cars = car_cascade.detectMultiScale(gray,1.1,5)
-# for (x, y, w, h) in cars:
-#     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-# cv2.imshow('img', img)
-# cv2.waitKey(0) 
-
-
 #Car detection in webcame
 import cv2
 cascade_src = 'cars.xml'
